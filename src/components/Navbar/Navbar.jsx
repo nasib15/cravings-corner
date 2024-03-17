f438ebaadaa4cedd90227f0748a5988b04d38f89
knh-nehal <nasibhossain57@gmail.com>
2024-03-17 08:36:15 +0600

    responsive with some fixes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,11 +14,27 @@ const Navbar = () => {
           <a className="btn btn-ghost text-lg font-medium">Search</a>
         </div>
         <div className="navbar-end gap-2">
-          <div>
+          <div className="relative">
+            <button className="btn btn-ghost btn-circle absolute lg:block hidden pl-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
             <input
               type="text"
               placeholder="Search"
-              className="input input-bordered w-24 md:w-auto hidden lg:block"
+              className="input input-bordered rounded-3xl w-24 md:w-auto hidden lg:block pl-11"
             />
           </div>
           <div>
